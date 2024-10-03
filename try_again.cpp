8ea029c21d7aa128f0260748a669a89c77d7670e
rafaellezama <lr43469@mail.harpercollege.edu>
2024-10-03 14:24:51 -0500

    Commit

--- try_again.cpp
+++ try_again.cpp
@@ -1,24 +1,32 @@
-#include <iostream>
+# include <iostream>
 using namespace std;
 
 void print_error_message_to_console();
-int prompt_and_read(int);
-
+int input_is_valid(int);
+int prompt_and_read();
 
 int main(){
-  prompt_and_read(input);
-  int input = prompt_and_read();
-  cout << input;
+  int value = prompt_and_read();
+  input_is_valid(value);
   return 0;
 }
 
-void print_error_message_to_console(){
-  cout << "Your value is invalid" <<endl;
-}
 int prompt_and_read(){
-  cout << "Please enter a value: ";
-  cin >> value;
-  cout << "The value chosen by the user is " << value;
-  cout <<endl;  
-  return value;
+  int input;
+  cout << "Enter a value: ";
+  cin >> input;
+  return input;
+}
+
+void print_error_message_to_console(){
+  cout << "Your value is invalid" << endl;
+}
+        //fix this validation function
+int input_is_valid(int value){
+  if (value >= 0 || value <= 100){
+    cout << "The value chosen by the user is " << value;
+    cout << endl;
+  } else{
+    cout << endl;
+  }
 }
